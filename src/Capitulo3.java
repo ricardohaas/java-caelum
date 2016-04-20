afb806a63183c1db839a491bf3ef11759681af2a
ricardo <ricardo@rica.home>
2016-04-20 20:09:18 -0300

    First commit

---
+++ src/Capitulo3.java
@@ -0,0 +1,13 @@
+
+public class Capitulo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;
+		
+		//System.out.println( i / 0);
+		System.out.println( i / 0.0);
+
+	}
+
+}
